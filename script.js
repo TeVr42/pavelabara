732407b670500ff8a533c11a80413b5b646c8baf
Tereza_Vrbova1 <tereza.vrbova1@tul.cz>
2024-03-14 23:02:53 +0100

    Resposivity add

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var cdays = document.getElementById("cdays");
+var tdays = document.getElementById("tdays");
 var chours = document.getElementById("chours");
+var thours = document.getElementById("thours");
 var cmin = document.getElementById("cminutes");
+var tmin = document.getElementById("tminutes");
 var csec = document.getElementById("cseconds");
+var tsec = document.getElementById("tseconds");
 
 var weddingDate = new Date('06/22/2024 1:00 PM');
 
@@ -29,10 +33,43 @@ function setCountdown() {
     chours.innerHTML = hours;
     cmin.innerHTML = minutes;
     csec.innerHTML = seconds;
+
+    if (days < 5 && days >= 2) {
+        tdays.innerHTML = "dny";
+    } else if (days == 1) {
+        tdays.innerHTML = "den";
+    } else {
+        tdays.innerHTML = "dní";
+    }
+
+    if (hours < 5 && hours >= 2) {
+        thours.innerHTML = "hodiny";
+    } else if (hours == 1) {
+        thours.innerHTML = "hodina";
+    } else {
+        thours.innerHTML = "hodin";
+    }
+
+    if (minutes < 5 && minutes >= 2) {
+        tmin.innerHTML = "minuty";
+    } else if (minutes == 1) {
+        tmin.innerHTML = "minuta";
+    } else {
+        tmin.innerHTML = "minut";
+    }
+
+    if (seconds < 5 && seconds >= 2) {
+        tsec.innerHTML = "sekundy";
+    } else if (seconds == 1) {
+        tsec.innerHTML = "sekunda";
+    } else {
+        tsec.innerHTML = "sekund";
+    }
+
 }
 
 function actualizeTime() {
     setCountdown();
-    countdown = setInterval(setCountdown, 1000);
+    countdown = setInterval(setCountdown, 500);
 }
 
